82cdedabc8e9b441b9d02a70497d305988c96259
tahira-sta <syedatahiraanwar.sta@gmail.com>
2019-09-14 00:18:08 +0500

    side menu fix

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -5,7 +5,7 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import undefined = require('firebase/empty-import');
+// import undefined = require('firebase/empty-import');
 
 @Component({
   selector: 'app-login',
@@ -13,24 +13,24 @@ import undefined = require('firebase/empty-import');
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-email;
-password;
-remember=false;
+  email;
+  password;
+  remember = false;
   loading: any;
   constructor(
     private router: Router,
     private fb: Facebook,
     public loadingCtrl: LoadingController,
     private fireAuth: AngularFireAuth,
-    private auth:AuthService
+    private auth: AuthService
   ) {
-let a = localStorage.getItem('softemail');
-let b= localStorage.getItem('softpassword');
-console.log(a);
-if(a!=null && b!=null){
-  this.email=a;
-  this.password=b
-}
+    let a = localStorage.getItem('softemail');
+    let b = localStorage.getItem('softpassword');
+    console.log(a);
+    if (a != null && b != null) {
+      this.email = a;
+      this.password = b
+    }
   }
 
   async ngOnInit() {
@@ -46,17 +46,17 @@ if(a!=null && b!=null){
 
 
   async fblogin() {
-  
+
 
     this.fb.login(['public_profile', 'email'])
-    .then((res: FacebookLoginResponse) => {
-  
-      const facebookCredential = firebase.auth.FacebookAuthProvider
+      .then((res: FacebookLoginResponse) => {
+
+        const facebookCredential = firebase.auth.FacebookAuthProvider
           .credential(res.authResponse.accessToken);
-console.log(facebookCredential);
+        console.log(facebookCredential);
         // this.auth.facebookLogin(facebookCredential)
         // .then( success => { 
-      
+
         //   let data = {
         //     email: success.user.email,
         //     password: '',
@@ -64,15 +64,15 @@ console.log(facebookCredential);
         //     dob: '',
         //     status: 'inactive'
         //   };
-      
+
         // }, err =>{
-        
+
         //   alert('Failed To Login.');
         // });
 
-    },err =>{
-      alert('Failed To Login.');
-    });
+      }, err => {
+        alert('Failed To Login.');
+      });
 
     this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
   }
@@ -88,24 +88,24 @@ console.log(facebookCredential);
   }
   onLoginError(err) {
     console.log(err);
-  } 
-  login(){
+  }
+  login() {
     console.log(this.remember);
-if(this.email!+null && this.password!=null && this.email!='' && this.password!=''){
-  this.auth.login(this.email,this.password).then((res:any)=>{
-    localStorage.setItem('softUser',res.user.uid);
-    if(this.remember==true){
-      localStorage.setItem('softemail',this.email);
-      localStorage.setItem('softpassword',this.password);
-      this.router.navigate(["/home"]);
-    }else{
-      this.router.navigate(["/home"]);
+    if (this.email! + null && this.password != null && this.email != '' && this.password != '') {
+      this.auth.login(this.email, this.password).then((res: any) => {
+        localStorage.setItem('softUser', res.user.uid);
+        if (this.remember == true) {
+          localStorage.setItem('softemail', this.email);
+          localStorage.setItem('softpassword', this.password);
+          this.router.navigate(["/menu/home"]);
+        } else {
+          this.router.navigate(["/menu/home"]);
+        }
+
+
+      }).catch(err => {
+        console.log(err);
+      })
     }
-    
-  
-  }).catch(err=>{
-    console.log(err);
-  })
-}
   }
 }
